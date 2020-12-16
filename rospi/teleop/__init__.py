058028e87e82b1cf5fc0f101a8d2b1369d422a92
Ben Morrow <ben@morrow.me.uk>
2020-12-16 09:48:56 +0000

    Get the joysticks working properly.
    They keyboard is now not working as well as before: changing speed while
    we are moving forward doesn't work properly, because no 'speed' event is
    sent. I think the keys need to be modelled more as a mini-joystick.

--- rospi/teleop/__init__.py
+++ rospi/teleop/__init__.py
@@ -2,8 +2,105 @@ from geometry_msgs.msg import Twist
 from geometry_msgs.msg import Vector3
 import rospy
 
-linV = 0.4
-angV = 2.0
+# Grr python2 rubbish
+__metaclass__ = type
+
+# An Action is a command sent from a control interface
+class Action:
+    __slots__   = ["type"]
+
+    def __init__ (self, type):
+        self.type   = type
+
+# A ControlAction is an adjustment to a ControlPoint (see below). This
+# adjustment can be absolute or relative and is scaled to the range 
+# [-1, 1].
+class ControlAction (Action):
+    __slots__   = ["value", "absolute"]
+
+    def __init__ (self, value, absolute=False, **kw):
+        super(ControlAction, self).__init__(**kw)
+
+        self.value      = value
+        self.absolute   = absolute
+
+def clamp (v, a, b):
+    if v < a:
+        return a
+    if v > b:
+        return b
+    return v
+
+# A ControlPoint is a value that can be changed by a control interface.
+# It can take positive or negative values up to a certain maximum.
+class ControlPoint:
+    __slots__ = [
+        "value",    # The current value of the control point
+        "step",     # The size of a relative change
+        "adj",      # The steps to change .step by
+        "max",      # The maximim allowed value
+    ]
+
+    def __init__ (self, value, step, adjust, max):
+        self.value  = value
+        self.step   = step
+        self.adj    = adjust
+        self.max    = max
+
+    # Use a control message to change the current value.
+    def apply (self, control):
+        if control.absolute:
+            self.value = control.value * self.max
+        else:
+            v           = self.value + control.value * self.step
+            self.value  = clamp(v, -self.max, self.max)
+
+    # Use a control message to change the current step size.
+    def adjust (self, control):
+        if control.absolute:
+            self.step   = control.value * self.max
+        else:
+            s           = self.step + control.value * self.adj
+            self.step = clamp(s, 0, self.max)
+
+class TwistController:
+    __slots__ = ["speed", "turn"]
+
+    def __init__ (self):
+        # XXX These need adjusting. The numbers are:
+        #   initial value
+        #   step applied by e.g. pressing W
+        #   adjustment to step e.g. by pressing arrows
+        #   max value
+        # 2.0 seems rather high for turn?
+        self.speed  = ControlPoint(0, 0.4, 0.1, 4.0)
+        self.turn   = ControlPoint(0, 0.5, 0.1, 2.0)
+
+    def stop (self):
+        self.speed.value    = 0
+        self.turn.value     = 0
+
+    def handle_action (self, action):
+        if action.type == "stop":
+            self.stop()
+
+        elif action.type == "speed":
+            self.speed.apply(action)
+
+        elif action.type == "turn":
+            self.turn.apply(action)
+
+        elif action.type == "adj-speed":
+            self.speed.adjust(action)
+
+        elif action.type == "adj-turn":
+            self.turn.adjust(action)
+
+    def to_twist (self):
+        return Twist(
+            linear=Vector3(self.speed.value, 0, 0),
+            angular=Vector3(0, 0, self.turn.value),
+        )
 
 def wait_for_subscribers(pub):
     i = 0
@@ -16,37 +113,14 @@ def wait_for_subscribers(pub):
     if rospy.is_shutdown():
         raise Exception("Got shutdown request before subscribers connected")
 
-def convert_demand(action):
-    cmd = Twist()
-    x = 0.0
-    phi = 0.0
-
-    if action == "forward":
-        x = linV
-    elif action == "backward":
-        x = -linV
-    elif action == "right":
-        phi = -angV
-        x = linV
-    elif action == "left":
-        phi = angV
-        x = linV
-
-    cmd.linear = Vector3(x, 0, 0)
-    cmd.angular = Vector3(0, 0, phi)
-
-    return cmd
-
 def run (win):
     win.setup()
-
-    buggy_cmd = Twist()
 
     pub = rospy.Publisher('demand_out', Twist, queue_size=1)
     rospy.init_node('pub')
 
-    rate = rospy.Rate(5)
-    state = 'stop'
+    rate    = rospy.Rate(5)
+    state   = TwistController()
 
     try:
         wait_for_subscribers(pub)
@@ -56,30 +130,15 @@ def run (win):
         while not rospy.is_shutdown() and running:
 
             for action in win.get_actions():
-
-                if action == "quit":
-                    state   = "stop"
+                if action.type == "quit":
+                    state.stop()
                     print("Exiting program")
                     running = False
-
-                elif action == "faster":
-                    linV += 0.1
-
-                elif action == "slower":
-                    linV -= 0.1
-
-                elif action == "turn-faster":
-                    angV += 0.5
-
-                elif action == "turn-slower":
-                    angV -= 0.5
-
                 else:
-                    state = action
+                    state.handle_action(action)
 
             win.update()
-            buggy_cmd = convert_demand(state)
-            pub.publish(buggy_cmd)
+            pub.publish(state.to_twist())
             rate.sleep()
     finally:
         win.quit()
--- rospi/teleop/xpygame.py
+++ rospi/teleop/xpygame.py
@@ -1,32 +1,67 @@
-import pygame
+import  pygame
+from    rospi.teleop    import Action, ControlAction
+
+# Grr python2 rubbish
+__metaclass__ = type
+
+DEADZONE    = 0.05
+
+class JoyAxis:
+    __slots__   = ["value", "action", "scale"]
+
+    def __init__ (self, action, scale=1.0):
+        self.value  = 0.0
+        self.action = action
+        self.scale  = scale
+
+    def handle_value (self, value):
+        if value < DEADZONE and value > -DEADZONE:
+            value   = 0.0
+        else:
+            value   = round(value * self.scale, 4)
+
+        if value == self.value:
+            return []
+        self.value  = value
+
+        return [ControlAction(
+            type=self.action,
+            value=value, 
+            absolute=True)]
 
 # This implements the pygame interface. This can handle key-up events,
 # so the motors stop as soon as you release the key. It create a tiny
 # window just to accept events.
 class PygameInterface:
     keydown = {
-        pygame.K_ESCAPE:        "quit",
-        pygame.K_SPACE:         "stop",
-        pygame.K_w:             "forward",
-        pygame.K_s:             "backward",
-        pygame.K_a:             "left",
-        pygame.K_d:             "right",
-        pygame.K_UP:            "faster",
-        pygame.K_DOWN:          "slower",
-        pygame.K_RIGHT:         "turn-faster",
-        pygame.K_LEFT:          "turn-slower",
+        pygame.K_ESCAPE:        Action(type="quit"),
+        pygame.K_SPACE:         Action(type="stop"),
+        pygame.K_w:             ControlAction(type="speed", value=1),
+        pygame.K_s:             ControlAction(type="speed", value=-1),
+        pygame.K_a:             ControlAction(type="turn", value=-1),
+        pygame.K_d:             ControlAction(type="turn", value=1),
+        pygame.K_UP:            ControlAction(type="adj-speed", value=1),
+        pygame.K_DOWN:          ControlAction(type="adj-speed", value=-1),
+        pygame.K_RIGHT:         ControlAction(type="adj-turn", value=1),
+        pygame.K_LEFT:          ControlAction(type="adj-turn", value=-1),
     }
 
     keyup = {
-        pygame.K_w:     "stop",
-        pygame.K_s:     "stop",
-        pygame.K_a:     "stop",
-        pygame.K_d:     "stop",
+        pygame.K_w:     ControlAction(type="speed", value=-1),
+        pygame.K_s:     ControlAction(type="speed", value=1),
+        pygame.K_a:     ControlAction(type="turn", value=1),
+        pygame.K_d:     ControlAction(type="turn", value=-1)
+    }
+
+    joy_axis = {
+        0:      JoyAxis(action="turn", scale=1.0),
+        1:      JoyAxis(action="speed", scale=-1.0),
     }
 
     def __init__ (self):
-        self.win    = None
-        self.joy    = None
+        self.win        = None
+        self.joy        = None
+        self.joy_active = False
 
     def setup (self):
         self.setup_display()
@@ -48,12 +83,19 @@ class PygameInterface:
     def quit (self):
         pygame.quit()
 
+    def handle_joy_event (self, event, actions):
+        if event.axis not in self.joy_axis:
+            return
+        axis    = self.joy_axis[event.axis]
+        value   = event.value
+
+
     def get_actions (self):
         actions = []
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                actions.append("quit")
+                actions.append(Action("quit"))
 
             if event.type == pygame.KEYDOWN:
                 if event.key in self.keydown:
@@ -64,16 +106,10 @@ class PygameInterface:
                     actions.append(self.keyup[event.key])
 
             if event.type == pygame.JOYAXISMOTION:
-                if event.axis == 0:
-                    if event.value > 0.1:
-                        actions.append("right")
-                    elif event.value < -0.1:
-                        actions.append("left")
-                elif event.axis == 1:
-                    if event.value > 0.1:
-                        actions.append("backward")
-                    elif event.value < -0.1:
-                        actions.append("forward")
+                if event.axis in self.joy_axis:
+                    actions.extend(
+                        self.joy_axis[event.axis].
+                            handle_value(event.value))
 
         return actions
 
